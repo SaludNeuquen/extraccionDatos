a861ccf46e9313f6fa6842240d10cd3205fc187d
nhuenchuman <natalia.huenchuman@gmail.com>
2017-01-03 15:07:32 -0300

    Se sube nuevo test para Heller

---
+++ testHeller2.ts
@@ -0,0 +1,49 @@
+import {servicioHeller} from './servicioHeller';
+import {libString} from './libString';
+import { servicioMongo } from './servicioMongo';
+
+
+var servMongo = new servicioMongo();
+var servicio = new servicioHeller();
+
+servicio.obtenerDatosHeller(/*1*/1,/*224202-166017*/35000)
+    .then((resultado) => {
+        if (resultado == null) {
+            console.log('No encontrado');
+        } else {
+            var lista;
+            lista = resultado;
+            var listaPacientesHeller = [];
+            console.log('Total de Pacientes en Heller', lista.length);
+            lista.forEach(registro => {
+                let pacienteHeller;
+                if (registro.NumeroDocumento.replace(/\"/g, "")) {
+                    listaPacientesHeller.push(pacienteHeller);
+                }
+            })
+            console.log(listaPacientesHeller.length);
+            if (listaPacientesHeller) {
+                console.log('Se guardan los pacientes de Heller');
+                servMongo.guardarPacientes(listaPacientesHeller, "pacienteHeller")
+                    .then((res => {
+                        console.log('Guardar Pacientes');
+                    }))
+                    .catch((err => {
+                        console.log('Error al guardar Pacientes', err)
+                        servMongo.guardarLog("logMigracion", { "idPacienteHeller": lista['id'], "Error": err.toString(), "Fecha": Date.now(), "Heller": true })
+                            .then((respuesta => {
+                                console.log('Guardar Log', 'Error al guardar paciente');
+                            }))
+                            .catch((err => {
+                                console.log('Error Paciente', lista['id'], err);
+                            }))
+                    }))
+
+            }
+
+        }
+    })
+    .catch((err) => {
+        console.error('Error**:' + err)
+    }
+    );
